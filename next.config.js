6491c6e2bd1e0f12ce06de5c35f05c0338dc9ffb
fabianhabil <fabianhabilramdhan@gmail.com>
2022-09-07 21:04:39 +0700

    next config js for next/image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['i.scdn.co']
+    }
+};
