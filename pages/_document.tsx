5e11be0b4ee8128deb7e3fe60226343dc636514e
fabianhabil <fabianhabilramdhan@gmail.com>
2022-09-07 21:07:47 +0700

    fixed lint problem

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+
     static async getInitialProps(ctx: any) {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
@@ -38,4 +39,5 @@ export default class MyDocument extends Document {
             </Html>
         );
     }
+
 }
